c0905ba244430699c6d16e1fcdf8d7889ef4b4b0
danaroInformatica <49897229+danaroInformatica@users.noreply.github.com>
2024-03-26 19:03:33 +0100

    Crud versión 2

--- laravel/resources/views/libros.blade.php
+++ laravel/resources/views/libros.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-Libros
+    a
 
 @endsection('content')
 
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,17 +1,24 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/libros', function() {
-    return view('libros'); 
+    return view('libros');
+
 });
 Route::get('/contacto', function() {
     return view('contacto'); 
 });
 
 
-Route::resource('/books', 'App\Http\Controllers\BookController@index');
+Route::resource('/books', BookController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
